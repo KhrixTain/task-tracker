1a956dc091418054f664c4f4f4390c5d74cd6e96
KhrixTain <christian_maccarino@outlook.com>
2023-10-10 23:39:31 -0300

    feat: Hello World message

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+const msg: Ref<string> = ref('Hello world!')
+</script>
+
+<template>
+  {{ msg }}
+</template>
